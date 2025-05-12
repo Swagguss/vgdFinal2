be1639791b7300323aa0e2425514c2cb0c100153
Swaggus <tromnationzac@gmail.com>
2025-05-12 14:01:51 -0700

    rope

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Camera cam;
-    [Header("Bones/Rigidbodies")]
-    public GameObject thighJoint;
-    public GameObject calfJoint;
-    public GameObject footJoint;
-    private Rigidbody2D thighRB;
-    private Rigidbody2D calfRB;
-    private Rigidbody2D footRB;
+
+    public GameObject thighJoint, calfJoint, footJoint;
+    private Rigidbody2D thighRB, calfRB, footRB;
     private Foot foot;
 
-    [Header("IK helpers")]
-    public Transform targetPos;
-    public Transform mouseTest; // debug sphere
-    public Transform thighWish; // Keeps thigh upright
+    public Transform targetPos, mouseTest, thighWish;
 
-    [Header("PD gains")]
-    public float Kp = 600f;
-    public float Kd = 80f;
-    public float torqueCap = 2000f;
-    public float maxJumpForce = 200f;
+    public float Kp = 600f, Kd = 80f, torqueCap = 2000f, maxJumpForce = 200f;
+
+    [Header("Rope anchor")]
+    public float anchorRange = 1.5f;
+    public LayerMask anchorLayerMask;
+    public Transform selector;
+
+    private RopeController currentRope = null;
+    private int currentSpan = -1;
 
     private float lenThigh, lenCalf;
     private float thighBaseRotation, calfBaseRotation;
-
     private float desiredThighDeg, desiredCalfDeg;
-    private Vector2 wishPos = Vector2.zero;
+    private Vector2 wishPos;
 
-    void Start()
+    private Vector2 prevWishPos = Vector2.zero;
+    private bool isCharging = false;
+    private float chargeDepth = 0f;
+    public float pressDeadZone = 0.1f;
+
+
+    private void Start()
     {
         thighRB = thighJoint.GetComponent<Rigidbody2D>();
         calfRB = calfJoint.GetComponent<Rigidbody2D>();
@@ -44,13 +46,99 @@ public class PlayerController : MonoBehaviour
         calfBaseRotation = calfJoint.transform.localEulerAngles.z;
     }
 
+    void TryToggleAnchor()
+    {
+        if (currentRope != null)
+        {
+            currentRope.hangingSectionIndex = -1;
+            currentRope = null;
+            currentSpan = -1;
+            return;
+        }
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(footJoint.transform.position,
+                                                       anchorRange,
+                                                       anchorLayerMask);
+
+        RopeController.SpanCollider bestSpan = null;
+        float bestDistSqr = float.PositiveInfinity;
+
+        foreach (var hit in hits)
+        {
+            RopeController.SpanCollider span = hit.GetComponent<RopeController.SpanCollider>();
+            if (span == null) continue;
+
+            float d2 = ((Vector2)footJoint.transform.position - (Vector2)hit.transform.position).sqrMagnitude;
+            if (d2 < bestDistSqr)
+            {
+                bestSpan = span;
+                bestDistSqr = d2;
+            }
+        }
+
+        if (bestSpan == null) return;
+
+        RopeController rope = bestSpan.GetComponentInParent<RopeController>();
+        if (rope == null) return;
+
+        rope.whatIsHangingFromTheRope = footJoint.transform;
+        rope.hangingSectionIndex = bestSpan.SpanIndex;
+
+        currentRope = rope;
+        currentSpan = bestSpan.SpanIndex;
+    }
+
     void Update()
     {
-        if (Input.GetMouseButton(0))
-            wishPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        UpdateSelectorGizmo();
+
+        if (Input.GetMouseButtonDown(0))
+            TryToggleAnchor();
+
+        wishPos = cam.ScreenToWorldPoint(Input.mousePosition);
         mouseTest.position = wishPos;
         targetPos.position = wishPos;
 
+        SolveTwoBoneIK();
+    }
+
+    void UpdateSelectorGizmo()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(footJoint.transform.position,
+                                                       anchorRange,
+                                                       anchorLayerMask);
+
+        if (hits.Length == 0)
+        {
+            selector.gameObject.SetActive(false);
+            return;
+        }
+
+        RopeController.SpanCollider closest = null;
+        float best = float.PositiveInfinity;
+        foreach (var h in hits)
+        {
+            var sc = h.GetComponent<RopeController.SpanCollider>();
+            if (sc == null) continue;
+
+            float d2 = ((Vector2)footJoint.transform.position - (Vector2)h.transform.position).sqrMagnitude;
+            if (d2 < best) { best = d2; closest = sc; }
+        }
+
+        if (closest == null)
+        {
+            selector.gameObject.SetActive(false);
+            return;
+        }
+
+        selector.gameObject.SetActive(true);
+        selector.position = new Vector3(closest.transform.position.x,
+                                        closest.transform.position.y,
+                                        -5f);
+    }
+
+    void SolveTwoBoneIK()
+    {
         float lenTarget = Vector2.Distance(thighJoint.transform.position, targetPos.position);
         Vector2 diff = (Vector2)targetPos.position - (Vector2)thighJoint.transform.position;
         float atan = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - 90f;
@@ -65,9 +153,9 @@ public class PlayerController : MonoBehaviour
         else
         {
             float a = Mathf.Acos(Mathf.Clamp((lenTarget * lenTarget + lenThigh * lenThigh - lenCalf * lenCalf) /
-                                               (2f * lenTarget * lenThigh), -1f, 1f)) * Mathf.Rad2Deg;
+                                             (2f * lenTarget * lenThigh), -1f, 1f)) * Mathf.Rad2Deg;
             float b = Mathf.Acos(Mathf.Clamp((lenCalf * lenCalf + lenThigh * lenThigh - lenTarget * lenTarget) /
-                                               (2f * lenCalf * lenThigh), -1f, 1f)) * Mathf.Rad2Deg;
+                                             (2f * lenCalf * lenThigh), -1f, 1f)) * Mathf.Rad2Deg;
 
             float thighA = atan - a;
             float calfA = 180f - b;
@@ -75,84 +163,56 @@ public class PlayerController : MonoBehaviour
             float thighB = atan + a + 180f;
             float calfB = -(180f - b);
 
-            if (calfA <= 0f)
-            {
-                joint0 = thighA;
-                joint1 = calfA;
-            }
-            else
-            {
-                joint0 = thighB;
-                joint1 = calfB;
-            }
+            if (calfA <= 0f) { joint0 = thighA; joint1 = calfA; }
+            else { joint0 = thighB; joint1 = calfB; }
         }
 
         desiredThighDeg = thighBaseRotation + joint0;
         desiredCalfDeg = calfBaseRotation + joint1;
     }
 
-    private Vector2 prevWishPos = Vector2.zero;
-    private bool isCharging = false;
-    private float chargeDepth = 0f;
-    public float pressDeadZone = 0.1f;
 
     void FixedUpdate()
     {
         ApplyPD(thighRB, desiredThighDeg);
         ApplyPD(calfRB, desiredThighDeg + desiredCalfDeg);
-        if (foot.collision != null)
+
+        if (foot.collision != null && foot.collision.contacts.Length > 0)
         {
-            if (foot.collision.contacts.Length > 0)
+            Vector2 n = foot.collision.contacts[0].normal;
+            ApplyPD(footRB, Mathf.Atan2(n.y, n.x) * Mathf.Rad2Deg);
+
+            if (foot.isGrounded)
             {
-                Vector2 n = foot.collision.contacts[0].normal;
-                ApplyPD(footRB, Mathf.Atan2(n.y, n.x) * Mathf.Rad2Deg);
-                if (foot.isGrounded)
+                Vector2 normal = n.normalized;
+                Vector2 mouseDelta = wishPos - prevWishPos;
+                float press = Vector2.Dot(mouseDelta, normal);
+
+                if (press > pressDeadZone) { isCharging = true; chargeDepth += press; }
+                bool released = isCharging && press < -pressDeadZone;
+                if (released)
                 {
-                    Vector2 normal = foot.collision.contacts[0].normal.normalized;
-
-                    Vector2 mouseDelta = wishPos - prevWishPos;
-                    float press = Vector2.Dot(mouseDelta, normal);
-
-                    if (press > pressDeadZone)
-                    {
-                        isCharging = true;
-                        chargeDepth += press;
-                    }
-
-                    bool released = isCharging && press < -pressDeadZone;
-
-                    if (released)
-                    {
-                        Jump(chargeDepth, -mouseDelta);
-                        chargeDepth = 0f;
-                        isCharging = false;
-                    }
+                    Jump(chargeDepth, -mouseDelta);
+                    chargeDepth = 0f; isCharging = false;
                 }
             }
         }
-        else
-        {
-            chargeDepth = 0f;
-            isCharging = false;
-        }
+        else { chargeDepth = 0f; isCharging = false; }
 
         prevWishPos = wishPos;
     }
 
     void Jump(float depth, Vector2 direction)
     {
-        Vector2 impulse = direction*Mathf.Clamp(depth, 0f, maxJumpForce);
-
+        Vector2 impulse = direction * Mathf.Clamp(depth, 0f, maxJumpForce);
         thighRB.AddForce(impulse, ForceMode2D.Impulse);
         calfRB.AddForce(impulse, ForceMode2D.Impulse);
-
         StartCoroutine(BoostPD(0.12f));
     }
 
     IEnumerator BoostPD(float t)
     {
-        float kp0 = Kp;
-        Kp *= 2f;
+        float kp0 = Kp; Kp *= 2f;
         yield return new WaitForSeconds(t);
         Kp = kp0;
     }
@@ -160,12 +220,10 @@ public class PlayerController : MonoBehaviour
     void ApplyPD(Rigidbody2D rb, float desiredDeg)
     {
         float errorRad = Mathf.DeltaAngle(rb.rotation, desiredDeg) * Mathf.Deg2Rad;
-
         float velRad = rb.angularVelocity * Mathf.Deg2Rad;
 
         float torque = Kp * errorRad - Kd * velRad;
         torque = Mathf.Clamp(torque, -torqueCap, torqueCap);
-
         rb.AddTorque(torque, ForceMode2D.Force);
     }
 }
---
+++ Assets/RopeSection.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public struct RopeSection
+{
+    public Vector3 pos;
+    public Vector3 vel;
+
+    public static readonly RopeSection zero = new RopeSection(Vector3.zero);
+
+    public RopeSection(Vector3 pos)
+    {
+        this.pos = pos;
+
+        this.vel = Vector3.zero;
+    }
+}
